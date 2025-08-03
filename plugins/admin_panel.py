99aa292692766d0c4d8229b46ad2406246b698cf
poper12 <samyakdhengle89@gmail.com>
2025-08-03 14:03:45 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -36,8 +36,8 @@ async def tutorial(bot: Client, message: Message):
         text=Txt.FILE_NAME_TXT.format(format_template=format_template),
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("• ᴏᴡɴᴇʀ", url="https://t.me/Union_Owner"),
-             InlineKeyboardButton("• ᴛᴜᴛᴏʀɪᴀʟ", url="https://t.me/Union_Owner")]
+            [InlineKeyboardButton("• ᴏᴡɴᴇʀ", url="https://t.me/aaru_2075"),
+             InlineKeyboardButton("• ᴛᴜᴛᴏʀɪᴀʟ", url="https://t.me/aaru_2075")]
         ])
     )
 
